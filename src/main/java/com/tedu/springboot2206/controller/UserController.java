ebdd0d2617c0a42a823f61f8de9ed6393ef4874b
yanghongtao <2460374290@qq.com>
2022-08-24 17:34:19 +0800

    变更

--- src/main/java/com/tedu/springboot2206/controller/UserController.java
+++ src/main/java/com/tedu/springboot2206/controller/UserController.java
@@ -20,23 +20,27 @@ public class UserController {
             userDir.mkdirs();
         }
     }
-
-
+    @RequestMapping("/deleteUser")
+    public void delete(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("开始处理删除用户！！！！！！！！");
+        String usernam = request.getParameter("username");
+        System.out.println("要删除的用户是：" + usernam);
+    }
 
 
     @RequestMapping("/userList")
-    public void userList(HttpServletRequest request,HttpServletResponse response){
+    public void userList(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("开始处理动态页面!!!!!!!");
 
         List<User> userList = new ArrayList<>();
         //获取users目录中所有obj文件
-        File[] subs = userDir.listFiles(f->f.getName().endsWith(".obj"));
+        File[] subs = userDir.listFiles(f -> f.getName().endsWith(".obj"));
         //将每个文件都返序列华得到User对象
-        for (File file : subs){
+        for (File file : subs) {
             try (
                     FileInputStream fis = new FileInputStream(file);
                     ObjectInputStream ois = new ObjectInputStream(fis);
-                    ){
+            ) {
                 User user = (User) ois.readObject();
                 userList.add(user);
             } catch (IOException | ClassNotFoundException e) {
@@ -46,48 +50,45 @@ public class UserController {
         System.out.println(userList);
 
 
+        try {
+            response.setContentType("text/html;charset=utf-8");
+            PrintWriter pw = response.getWriter();
+            pw.println("<!DOCTYPE html>");
+            pw.println("<html lang=\"en\">");
+            pw.println("<head>");
+            pw.println("<meta charset=\"UTF-8\">");
+            pw.println("<title>我的首页</title>");
+            pw.println("</head>");
+            pw.println("<body>");
+            pw.println("<center>");
+            pw.println("<h1>用户列表</h1>");
+            pw.println("<table border=\"1\">");
+            pw.println("<tr>");
+            pw.println("<td>用户名</td>");
+            pw.println("<td>密码</td>");
+            pw.println("<td>昵称</td>");
+            pw.println("<td>年龄</td>");
+            pw.println("<td>操作</td>");
+            pw.println("</tr>");
 
-            try {
-                response.setContentType("text/html;charset=utf-8");
-                PrintWriter pw = response.getWriter();
-                pw.println("<!DOCTYPE html>");
-                pw.println("<html lang=\"en\">");
-                pw.println("<head>");
-                pw.println("<meta charset=\"UTF-8\">");
-                pw.println("<title>我的首页</title>");
-                pw.println("</head>");
-                pw.println("<body>");
-                pw.println("<center>");
-                pw.println("<h1>用户列表</h1>");
-                pw.println("<table border=\"1\">");
+            for (User user : userList) {
                 pw.println("<tr>");
-                pw.println("<td>用户名</td>");
-                pw.println("<td>密码</td>");
-                pw.println("<td>昵称</td>");
-                pw.println("<td>年龄</td>");
-                pw.println("<td>操作</td>");
+                pw.println("<td>" + user.getUsername() + "</td>");
+                pw.println("<td>" + user.getPassword() + "</td>");
+                pw.println("<td>" + user.getNickname() + "</td>");
+                pw.println("<td>" + user.getAge() + "</td>");
+                pw.println("<td><a href=\"/deleteUser?username="+ user.getUsername() + "\">删除</a></td>");
                 pw.println("</tr>");
-
-                for (User user:userList){
-                    pw.println("<tr>");
-                    pw.println("<td>"+user.getUsername()+"</td>");
-                    pw.println("<td>"+user.getPassword()+"</td>");
-                    pw.println("<td>"+user.getNickname()+"</td>");
-                    pw.println("<td>"+user.getAge()+"</td>");
-                    pw.println("<td><a href=\"/deleteUser?username =" +user.getUsername()+"\">删除</a></td>");
-                    pw.println("</tr>");
-                }
-
-                pw.println("</table>");
-                pw.println("</center>");
-                pw.println("</body>");
-                pw.println("</html>");
-
-            } catch (IOException e) {
-                e.printStackTrace();
             }
 
+            pw.println("</table>");
+            pw.println("</center>");
+            pw.println("</body>");
+            pw.println("</html>");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
